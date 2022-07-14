fd2c62c9b8b264426b536141cfbca36e63bcec98
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-07-14 22:51:33 +0300

    use local installation of python package

--- docs/regenerate-docs.sh
+++ docs/regenerate-docs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-python3  /usr/local/lib/python3.9/site-packages/json_schema_for_humans/cli.py   ../json_schema/meta/prism-credentials.json
+JSFH_HOME=/Users/rssh/packages/json-schema-for-humans/json-schema-for-humans/json_schema_for_humans
+#JSFH_HOME=/usr/local/lib/python3.9/site-packages/json_schema_for_humans/
+python3  $JSFH_HOME/cli.py   ../json_schema/meta/prism-credentials.json
 git checkout gh-pages
 mv schema_doc.* generated
 git commit -a -m "regenerate prism credential docs"
