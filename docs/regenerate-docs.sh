1b299d5f19911dfec49d10d17a543ce2be5e5dea
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-07-14 19:48:59 +0300

    regenerate prism credential docs

--- docs/regenerate-docs.sh
+++ docs/regenerate-docs.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 python3  /usr/local/lib/python3.9/site-packages/json_schema_for_humans/cli.py   ../json_schema/meta/prism-credentials.json
+git checkout gh-pages
+mv schema_doc.* generated
+git commit -a -m "regenerate prism credential docs"
+git checkout main
