e5605141d279e54a9b8615aee22ed3301329762f
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2022-09-13 16:52:43 +0300

    implemented minimal test (not good yet).

--- tools/ts-schema-tool/tests/check-schema-definition-dir.test.ts
+++ tools/ts-schema-tool/tests/check-schema-definition-dir.test.ts
@@ -1,31 +1,16 @@
 import { callCli }  from './cliCaller'
 import * as fs from 'fs'
 
-test('for all positive schemas', async () => {
+test('check schema definitions: pos', async () => {
   const posDir = "../../examples/schemas/pos";
   const posSchemas = fs.readdirSync(posDir)
   for(const schemaFile of posSchemas) {
     const fname = `${posDir}/${schemaFile}`
-    console.log("scheck-schema-definition "+fname)
-    let result = await callCli(['check-schema-definition', '"../../examples/schemas/pos/PersonName.json"'], '.');
+    console.log("scheck-schema-definition " + fname)
+    const result = await callCli(['check-schema-definition', fname], '.');
     expect(result.stderr).toBe("");
     expect(result.stdout).toBe('ok\n');
     expect(result.code).toBe(0);
   }
 })
- 
 
-
-/*
-function cli(args, cwd) {
-    return new Promise(resolve => { 
-        exec(`node ${path.resolve('./dist/cli')} ${args.join(' ')}`,
-        { cwd }, 
-        (error, stdout, stderr) => { resolve({
-        code: error && error.code ? error.code : 0,
-        error,
-        stdout,
-        stderr })
-    })
-})}
-*/
