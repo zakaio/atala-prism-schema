886b0f44bf134a55f8c1e698de3f0a2fa37a5290
Security2431 <artem2431@gmail.com>
2022-11-25 13:11:20 +0200

    fix: additionalProperties in prism metaschema

--- tools/ts-schema-tool/tests/validate-credentials.test.ts
+++ tools/ts-schema-tool/tests/validate-credentials.test.ts
@@ -1,36 +1,39 @@
-import { callCli }  from './cliCaller'
+import { callCli } from './cliCaller'
 import { TestFsHelper } from './TestFsHelper'
 
+test('Validate credentials positive', async () => {
+  await TestFsHelper.forFilesIn("../../examples/schemas/pos", async (schemaPath, schemaNameJson) => {
+    const schemaName = schemaNameJson.split(".")[0]
+    const posCredentials = `../../examples/credentials/${schemaName}/pos`
 
+    await TestFsHelper.forFilesIn(posCredentials, async (credPath, credName) => {
+      console.log(`validate ${credPath} against ${schemaPath} (pos)`)
+      let result = await callCli(['validate-credential', schemaPath, credPath], '.');
 
+      if (result.code !== 0) {
+        console.log("error output:")
+        console.error(result.stderr)
+      }
 
-        test('Validate credentials positive', async () => {
-            await TestFsHelper.forFilesIn("../../examples/schemas/pos", async (schemaPath, schemaNameJson) => {
-                const schemaName = schemaNameJson.split(".")[0]
-                const posCredentials = `../../examples/credentials/${schemaName}/pos`
-                await TestFsHelper.forFilesIn(posCredentials, async (credPath, credName) => {
-                   console.log(`validate ${credPath} against ${schemaPath} (pos)`)
-                   let result = await callCli(['validate-credential', schemaPath, credPath], '.');
-                   if (result.code !== 0) {
-                      console.log("error output:")
-                      console.error(result.stderr)
-                   }
-                   expect(result.code).toBe(0)
-                })
-            })        
-    
-        })
+      expect(result.code).toBe(0)
+    })
+  })
 
-        test('Validate credentials negative', async () => {
-            await TestFsHelper.forFilesIn("../../examples/schemas/pos", async (schemaPath, schemaNameJson) => {
-                const schemaName = schemaNameJson.split(".")[0]
-                const negCredentials = `../../examples/credentials/${schemaName}/neg`
-                await TestFsHelper.forFilesIn(negCredentials, async (credPath, credName) => {
-                   console.log(`validate ${credPath} against ${schemaPath} (neg)`)
-                   let result = await callCli(['validate-credential', schemaPath, credPath], '.');
-                   expect(result.code).not.toBe(0)
-                })
-            })
-        })
+})
+
+test('Validate credentials negative', async () => {
+  await TestFsHelper.forFilesIn("../../examples/schemas/pos", async (schemaPath, schemaNameJson) => {
+    const schemaName = schemaNameJson.split(".")[0]
+    const negCredentials = `../../examples/credentials/${schemaName}/neg`
+
+    await TestFsHelper.forFilesIn(negCredentials, async (credPath, credName) => {
+      console.log(`validate ${credPath} against ${schemaPath} (neg)`)
+
+      let result = await callCli(['validate-credential', schemaPath, credPath], '.');
+
+      expect(result.code).not.toBe(0)
+    })
+  })
+})
 
 
